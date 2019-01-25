392d3ab77f7a6782d73df6a2108f2bcd2c306e16
lanfairy <1264894052@qq.com>
2019-01-25 17:55:49 +0800

    1.数据库创建

--- app/src/main/java/com/lanfairy/elly/androidsummary/DataStore/DataStoreAty.java
+++ app/src/main/java/com/lanfairy/elly/androidsummary/DataStore/DataStoreAty.java
@@ -1,11 +1,13 @@
 package com.lanfairy.elly.androidsummary.DataStore;
 
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.lanfairy.elly.androidsummary.DataStore.util.SqliteOpenHelperUtil;
 import com.lanfairy.elly.androidsummary.R;
 
 public class DataStoreAty extends AppCompatActivity  {
@@ -14,6 +16,9 @@ public class DataStoreAty extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_store);
+
+        SqliteOpenHelperUtil sqliteUtil = new SqliteOpenHelperUtil(DataStoreAty.this);
+        SQLiteDatabase database = sqliteUtil.getReadableDatabase();
     }
 
     public void btnOnClick(View view) {
